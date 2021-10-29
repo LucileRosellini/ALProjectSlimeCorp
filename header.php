d10df389f4b988e287d4bfd71f9dd7fe942ee186
Antoine Chemin <67341298+AntoineCheminPro@users.noreply.github.com>
2021-10-29 16:48:53 +0200

    header title fixed

--- header.php
+++ header.php
@@ -14,12 +14,12 @@
 
 
 <header class="bg-firstColor">
-    <div class="row justify-content-center ">
+    <div class="row justify-content-center">
         <div>
             <img class="display-block m-2 p-2" src="img\logo.png" alt="">
         </div>
-        <div>
-            <h1 id="titre" class="text-center py-3 text-success justify-content-center">SLIME CORP</h1>
+        <div class="d-flex">
+            <h1 id="titre" class="text-center py-3 text-success m-auto">SLIME CORP</h1>
         </div>
     </div>
 
