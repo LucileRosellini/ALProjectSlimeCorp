afebbdad161803afbb1b8ab15b526690a401fd21
Antoine Chemin <67341298+AntoineCheminPro@users.noreply.github.com>
2021-10-29 11:01:57 +0200

    header and footer file added

--- index.php
+++ index.php
@@ -6,14 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CDN Bootstrap-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">    <!-- css-->
-    <link rel="stylesheet" href="./style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    <link rel="stylesheet" href="./style.css">
     <title>SlimeCorp</title>
 </head>
 
 <body>
 
-  <!--?php require_once 'header.php' ?-->
+  <?php require_once 'header.php' ?>
 
     <main>
         <h1 id="titre" class="text-center py-3 text-success">SLIME CORP</h1>
@@ -33,7 +32,7 @@
 
     </main>
 
-    <!--?php require_once 'footer.php' ?-->
+    <?php require_once 'footer.php' ?>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"> </script>    <!--script-->
     <script src="js/script.js"></script>
