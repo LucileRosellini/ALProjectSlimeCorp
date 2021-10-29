8c8f92ef526f241a48b328af72b13fa51a9d918f
LucileRosellini <60214845+LucileRosellini@users.noreply.github.com>
2021-10-29 10:32:39 +0200

    init

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CDN Bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">    <!-- css-->
+    <link rel="stylesheet" href="./style.css">
+    <title>SlimeCorp</title>
+</head>
+
+<body>
+
+  <!--?php require_once 'header.php' ?-->
+
+    <main>
+        <h1 id="titre" class="text-center py-3 text-success">SLIME CORP</h1>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
+
+    </main>
+
+    <!--?php require_once 'footer.php' ?-->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"> </script>    <!--script-->
+    <script src="js/script.js"></script>
+</body>
+
+</html>
